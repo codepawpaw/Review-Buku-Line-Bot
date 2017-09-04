2d1095a48294a3a27f6a1e96f9b4228dc45559e2
jonathan <jonathannael12@gmail.com>
2017-09-04 22:14:42 +0700

    WIP

--- app/controllers/main_bot_controller.rb
+++ app/controllers/main_bot_controller.rb
@@ -15,20 +15,20 @@ class MainBotController < ApplicationController
 	end
 
 	def index
-	  body = request.body.read
+	  # body = request.body.read
 
-	  signature = request.env['HTTP_X_LINE_SIGNATURE']
-	  unless client.validate_signature(body, signature)
-	    error 400 do 'Bad Request' end
-	  end
+	  # signature = request.env['HTTP_X_LINE_SIGNATURE']
+	  # unless client.validate_signature(body, signature)
+	  #   error 400 do 'Bad Request' end
+	  # end
 
-	  events = client.parse_events_from(body)
-	  events.each { |event|
-	    case event
-	    when Line::Bot::Event::Message
+	  # events = client.parse_events_from(body)
+	  # events.each { |event|
+	  #   case event
+	  #   when Line::Bot::Event::Message
 
-	      case event.type
-	      when Line::Bot::Event::MessageType::Text
+	  #     case event.type
+	  #     when Line::Bot::Event::MessageType::Text
 	  #       url = "https://www.goodreads.com/search/index.xml?key=hfQfAv9UN6tjGlTMKj0qtw&q=" + event.message['text']
 			# response = RestClient.get url
 
@@ -51,19 +51,47 @@ class MainBotController < ApplicationController
 			#     }
 			#     client.reply_message(event['replyToken'], message)
 			# end
-			message = {
-		          type: 'text',
-		          text: 'asd'
-		    }
-		    client.reply_message(event['replyToken'], message)
+			# message = {
+		 #          type: 'text',
+		 #          text: 'asd'
+		 #    }
+		 #    client.reply_message(event['replyToken'], message)
 
-	      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-	        response = client.get_message_content(event.message['id'])
-	        tf = Tempfile.open("content")
-	        tf.write(response.body)
-	      end
-	    end
-	  }
+	  #     when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+	  #       response = client.get_message_content(event.message['id'])
+	  #       tf = Tempfile.open("content")
+	  #       tf.write(response.body)
+	  #     end
+	  #   end
+	  # }
+	  		body = request.body.read
+	  		events = client.parse_events_from(body)
+
+	  		events.each { |event|
+
+		  		url = "https://www.goodreads.com/search/index.xml?key=hfQfAv9UN6tjGlTMKj0qtw&q=" + event.message['text']
+				response = RestClient.get url
+
+				doc = Nokogiri::XML(response)
+			    doc.search('//work').each do |element|
+			    	book_detail = element.at('best_book')
+			    	book = {
+			    		:id => element.at('id').text,
+			    		:publication_year => element.at('original_publication_year').text,
+			    		:rating => element.at('average_rati	ng').text,
+			    		:ratings_count => element.at('ratings_count').text,
+			    		:title => book_detail.at('title').text,
+			    		:author => book_detail.at('author').at('name').text,
+			    		:image_url => book_detail.at('image_url').text
+			    	}
+
+					message = {
+			          type: 'text',
+			          text: book[:rating]
+				    }
+				    client.reply_message(event['replyToken'], message)
+				end
+			}
 
 	  json_response([])
 	end
