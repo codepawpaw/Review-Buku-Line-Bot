748159826e06b1f98247f0738b59e93bf6f7af5f
jonathan <jonathannael12@gmail.com>
2017-09-05 22:44:57 +0700

    WIP

--- app/controllers/main_bot_controller.rb
+++ app/controllers/main_bot_controller.rb
@@ -63,7 +63,8 @@ class MainBotController < ApplicationController
 
 				text_message = {
 		          type: 'text',
-		          text: book[:title] + "\n" + book[:rating]
+		          text: book[:title] + "\n" + "by " + book[:author] + "\n" + book[:rating] + "avg rating - " 
+		          		+ book[:ratings_count] + "\n" + "publication year : " + book[:publication_year]
 			    }
 
 			    @messages << text_message
