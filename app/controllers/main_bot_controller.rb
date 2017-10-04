bf0575f48a2380e46f16cfef03094550d1d4ab3e
jonathan <jonathannael12@gmail.com>
2017-10-05 04:49:51 +0700

    enabling bot

--- app/controllers/main_bot_controller.rb
+++ app/controllers/main_bot_controller.rb
@@ -27,53 +27,48 @@ class MainBotController < ApplicationController
 	  events.each { |event|
 
 	      if event.type == Line::Bot::Event::MessageType::Text
-	  #       url = "https://www.goodreads.com/search/index.xml?key=hfQfAv9UN6tjGlTMKj0qtw&q=" + event.message['text']
-			# response = RestClient.get url
+	        url = "https://www.goodreads.com/search/index.xml?key=hfQfAv9UN6tjGlTMKj0qtw&q=" + event.message['text']
+			response = RestClient.get url
 
-			# doc = Nokogiri::XML(response)
-			# @messages = []
-			# counter = 0
-		 #    doc.search('//work').each do |element|
-		 #    	p counter
-		 #    	if counter > 6 then
-		 #    		break
-		 #    	end
+			doc = Nokogiri::XML(response)
+			@messages = []
+			counter = 0
+		    doc.search('//work').each do |element|
+		    	p counter
+		    	if counter > 6 then
+		    		break
+		    	end
 
-		 #    	book_detail = element.at('best_book')
-		 #    	book = {
-		 #    		:id => element.at('id').text,
-		 #    		:publication_year => element.at('original_publication_year').text,
-		 #    		:rating => element.at('average_rating').text,
-		 #    		:ratings_count => element.at('ratings_count').text,
-		 #    		:title => book_detail.at('title').text,
-		 #    		:author => book_detail.at('author').at('name').text,
-		 #    		:image_url => book_detail.at('image_url').text,
-		 #    		:small_image_url => book_detail.at('small_image_url').text
-		 #    	}
+		    	book_detail = element.at('best_book')
+		    	book = {
+		    		:id => element.at('id').text,
+		    		:publication_year => element.at('original_publication_year').text,
+		    		:rating => element.at('average_rating').text,
+		    		:ratings_count => element.at('ratings_count').text,
+		    		:title => book_detail.at('title').text,
+		    		:author => book_detail.at('author').at('name').text,
+		    		:image_url => book_detail.at('image_url').text,
+		    		:small_image_url => book_detail.at('small_image_url').text
+		    	}
 
-		 #    	image_message = {
-		 #    		type: 'image',
-		 #    		originalContentUrl: book[:image_url],
-		 #    		previewImageUrl: book[:image_url]
-		 #    	}
+		    	image_message = {
+		    		type: 'image',
+		    		originalContentUrl: book[:image_url],
+		    		previewImageUrl: book[:image_url]
+		    	}
 
-		 #    	@messages << image_message
+		    	@messages << image_message
 
-			# 	text_message = {
-		 #          type: 'text',
-		 #          text: book[:title] + "\n" + "by " + book[:author] + "\n" + book[:rating] + " avg rating - " + book[:ratings_count] + "\n" + "publication year : " + book[:publication_year]
-			#     }
+				text_message = {
+		          type: 'text',
+		          text: book[:title] + "\n" + "by " + book[:author] + "\n" + book[:rating] + " avg rating - " + book[:ratings_count] + "\n" + "publication year : " + book[:publication_year]
+			    }
 
-			#     @messages << text_message
-			#     counter = counter + 1
+			    @messages << text_message
+			    counter = counter + 1
 			    
-			# end
-			# client.reply_message(event['replyToken'], @messages[0..3])
-			text_message = {
-		        type: 'text',
-		        text: 'test'
-			}
-	        client.reply_message(event['replyToken'], text_message)
+			end
+			client.reply_message(event['replyToken'], @messages[0..3])
 		  end
 	  }
 
